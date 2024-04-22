75e59c71fa6c2fb1dd3aad4b1f89759c7787ebc2
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-04-22 17:57:48 -0500

    wip

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Instalar Paper.js en el scope global
 paper.install(window);
 
 window.onload = function () {
@@ -6,12 +5,76 @@ window.onload = function () {
 
   const size = new Size(800, 600);
   view.viewSize = size;
+  let direction = 1;
 
-  var estrella = new Path.Star({
-    center: [300, 100],
-    points: 10,
-    radius1: 10,
-    radius2: 40,
-    fillColor: "green",
+  // vamos a hacer un perro
+
+  let cuerpo = new Path.Rectangle({
+    center: [300, 200],
+    size: [100, 50],
+    fillColor: "black",
   });
+  let pie1 = new Path.Rectangle({
+    center: [275, 225],
+    size: [20, 20],
+    fillColor: "black",
+  });
+  let pie2 = new Path.Rectangle({
+    center: [325, 225],
+    size: [20, 20],
+    fillColor: "black",
+  });
+  let cola = new Path.Rectangle({
+    center: [250, 195],
+    size: [40, 10],
+    fillColor: "black",
+  });
+
+  let cuerpoPerro = new Group([cuerpo, pie1, pie2, cola]);
+
+  let cabeza = new Path.Circle({
+    center: [350, 160],
+    radius: 30,
+    fillColor: "black",
+  });
+  let nariz = new Path.Ellipse({
+    center: [350, 160],
+    radius: [10, 7], // radioX y radioY
+    fillColor: "red",
+  });
+  let cabezaPerro = new Group([cabeza, nariz]);
+
+  // Los unimos a traves de un grupo
+  let perro = new Group([cuerpoPerro, cabezaPerro]);
+  let keys = {};
+  let tool = new paper.Tool();
+
+  // Para el movimiento del perro, vamos a usar las teclas de dirección
+  tool.onKeyDown = function (event) {
+    keys[event.key] = true;
+  };
+  tool.onKeyUp = function (event) {
+    keys[event.key] = false;
+  };
+
+  view.onFrame = function (event) {
+    if (keys["w"]) {
+      perro.position.y -= 10;
+    }
+    if (keys["s"]) {
+      perro.position.y += 10;
+    }
+    if (keys["a"]) {
+      perro.position.x -= 10;
+    }
+    if (keys["d"]) {
+      perro.position.x += 10;
+    }
+
+    //Para el movimiento de la cola
+    cola.rotate(3 * direction);
+    if (event.count % 15 === 0) {
+      direction *= -1;
+    }
+  };
 };
