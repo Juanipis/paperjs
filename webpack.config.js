0e404bbb3658c6d6092a6f6a5c4936dfebff29b7
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-04-23 14:26:44 -0500

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,9 +27,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      paper: "paper",
-    }),
     new HtmlWebpackPlugin({
       template: "./index.html", // Usar index.html en la raíz como plantilla
       filename: "index.html", // Nombre del archivo en la salida
